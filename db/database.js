c42d1762c9409a80acc947fe2cad8ab17896d734
yunusakca <yunnusakca@gmail.com>
2023-07-14 19:52:39 +0300

    AWS DB

--- db/database.js
+++ db/database.js
@@ -1,12 +1,14 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('sql7620068', 'sql7620068', '2y5yuY44mQ', {
-    host: 'sql7.freesqldatabase.com',
+const sequelize = new Sequelize('my_db', 'admin', '.wHatisHype123.', {
+    host: 'database-1.cqc4ntl04qys.eu-north-1.rds.amazonaws.com',
     dialect: 'mysql'
   });
 
 if (sequelize.authenticate()) {
     console.log('Connection has been established successfully.');
+} else {
+ console.log("Couldn't connect to the database.")
 }
 
 module.exports = sequelize;
